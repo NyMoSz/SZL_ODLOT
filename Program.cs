dd4854398c664c51971e14cbc1a71256aee0db79
nymosz <lapniewskiszymon7@gmail.com>
2023-04-04 15:08:49 +0200

    menu

--- Program.cs
+++ Program.cs
@@ -1,18 +1,92 @@
-﻿namespace ConsoleApp8
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
 {
-    internal class Program
+    internal class Menu
     {
-        static void Main(string[] args)
+        static string[] pozycjeMenu = { "Opcja 1", "Opcja 2", "Opcja 3", "Opcja 4", "Opcja 5", "Koniec" };
+        static int aktywnaPozycjaMenu = 0;
+
+        public static void StartMenu()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "elo zelo";
+            Console.CursorVisible = false;
+            while (true)
+            {
+                PokazMenu();
+                WybieranieOpcji();
+                UruchomOpcje();
+            }
+            static void PokazMenu()
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("hejka naklejka");
+                Console.WriteLine();
+                for (int i = 0; i < pozycjeMenu.Length; i++)
+                {
+                    if (i == aktywnaPozycjaMenu)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("{0,-35}", pozycjeMenu[i]);
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    }
+                    else
+                    {
+                        Console.WriteLine(pozycjeMenu[i]);
+                    }
+                }
+            }
+            static void WybieranieOpcji()
+            {
+                do
+                {
+                    ConsoleKeyInfo klawisz = Console.ReadKey();
+                    if (klawisz.Key == ConsoleKey.UpArrow)
+                    {
+                        aktywnaPozycjaMenu = (aktywnaPozycjaMenu > 0) ? aktywnaPozycjaMenu - 1 : pozycjeMenu.Length - 1;
+                        PokazMenu();
+                    }
+                    else if (klawisz.Key == ConsoleKey.DownArrow)
+                    {
+                        aktywnaPozycjaMenu = (aktywnaPozycjaMenu + 1) % pozycjeMenu.Length;
+                        PokazMenu();
+                    }
+                    else if (klawisz.Key == ConsoleKey.Escape)
+                    {
+                        aktywnaPozycjaMenu = pozycjeMenu.Length - 1;
+                        break;
+                    }
+                    else if (klawisz.Key == ConsoleKey.Enter)
+                        break;
 
-            Console.WriteLine("Edsaasd!");
-
-            Console.WriteLine("elo zelo");
-
-            Console.WriteLine("gajewska");
-
-            Console.WriteLine("german");
+                } while (true);
+            }
+            static void UruchomOpcje()
+            {
+                switch (aktywnaPozycjaMenu)
+                {
+                    case 0: Console.Clear(); opcjawBudowie(); break;
+                    case 1: Console.Clear(); opcjawBudowie(); break;
+                    case 2: Console.Clear(); opcjawBudowie(); break;
+                    case 3: Console.Clear(); opcjawBudowie(); break;
+                    case 4: Console.Clear(); opcjawBudowie(); break;
+                    case 5: Environment.Exit(0); break;
+                }
+            }
+            static void opcjawBudowie()
+            {
+                Console.SetCursorPosition(12, 4);
+                Console.Write("Witam, dzien dobry");
+                Console.ReadKey();
+            }
         }
     }
 }
